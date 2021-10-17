ee2aa6338b641d7c67a8105f8fbefef389f167c7
Wangxin555 <wangx555@mail2.sysu.edu.cn>
2021-10-17 16:16:10 -0400

    Add a script for exporting results

---
+++ saveResults.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// export the search results in a txt file
+func saveSearchResult(filename string, papers []Paper) {
+	outFile, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Sorry: couldn’t create the file!")
+	}
+	defer outFile.Close()
+
+	fmt.Fprintln(outFile, "PMID"+"\t"+"Title"+"\t"+"Abstract")
+
+	for _, data := range papers {
+		fmt.Fprintln(outFile, data.PMID+"\t"+data.title+"\t",
+			data.abstract)
+	}
+	fmt.Println("Successfully write to txt file!")
+}
