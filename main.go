37db5e2dd0cfff16b49c5d06792a7a1176ddce5e
Wangxin555 <wangx555@mail2.sysu.edu.cn>
2021-10-20 15:56:14 -0400

    Update main.go

--- main.go
+++ main.go
@@ -16,8 +16,8 @@ func main() {
 	// numPaper refer to the number of paper the user wants to consider
 	flag.IntVar(&numPaper, "numPaper", 100, "Number of paper to be considered")
 	// keyWords are the words that user wants to search
-	// multiple words should use , to separate
-	flag.StringVar(&keyWords, "keyWords", "trending", "Words for searching")
+	// multiple words should use , to separate, such as gene,cancer
+	flag.StringVar(&keyWords, "keyWords", "trending", "Key words for searching")
 	flag.StringVar(&searchOutputFile, "searchFileName", "searchOutput.txt",
 		"A txt file to store the search output")
 	flag.BoolVar(&searchOutput, "exportSearchOutput", false,
@@ -26,12 +26,12 @@ func main() {
 
 	papers := FetchPaperInfo(keyWords, numPaper)
 
-	sents := analyzePaperInfo(papers, true, 10)
+	words := analyzePaperInfo(papers, true, 10)
 
 	if searchOutput {
 		saveSearchResult(searchOutputFile, papers)
 	}
 
-	fmt.Println(sents, len(sents))
+	fmt.Println(words)
 
 }
