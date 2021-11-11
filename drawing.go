819beaab6d51b107f0e60454bea70e9d112a6f1d
Wangxin555 <wangx555@mail2.sysu.edu.cn>
2021-11-10 23:12:05 -0500

    Add drawing function to draw wordcloud

---
+++ drawing.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+import "wordclouds"
+
+func drawWordCloud(wordFreq map[string]int) {
+	w := wordclouds.NewWordcloud(
+		(wordFreq,
+		wordclouds.FontFile("fonts/myfont.ttf"),
+		wordclouds.Height(2048),
+		wordclouds.Width(2048),
+	)
+}
+*/
