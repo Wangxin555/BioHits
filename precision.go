fd0db0b63ce02cb8700d8078221b66aab157f826
Wangxin555 <wangx555@mail2.sysu.edu.cn>
2021-11-10 23:12:53 -0500

    precision.go script aims to increase the precision of BioHits

---
+++ precision.go
@@ -0,0 +1,10 @@
+package main
+
+// Functions in this script aims to increase the precision of BioHits by
+// 1. removing "s" or "es" in Plurals, which transforms them into singulars
+// 2.
+
+// HandlePlural
+func HandlePlural(wordFreq map[string]int) map[string]int {
+
+}
