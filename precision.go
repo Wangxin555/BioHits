bdfc9e1932272b44658eb2b1d6f23c02aecfcaa3
Wangxin555 <wangx555@mail2.sysu.edu.cn>
2021-11-13 11:25:27 -0500

    Add code for processing adverb

--- precision.go
+++ precision.go
@@ -5,16 +5,16 @@ import (
 	"strings"
 )
 
-// Functions in this script aims to increase the precision of BioHits by
-// 1. removing "s" or "es" in Plurals, which transforms them into singulars
-// 2.
+// Function in this script aims to increase the precision of BioHits by
+// 1. removing "s" or "es" in Plurals, which transforms them into singulars noun
+// 2. removing "ly" in adv, which transform them into a normal sense verb
 
-// HandlePlural
-func HandlePlural(wordFreq map[string]int) map[string]int {
+// WordTransform
+func WordTransform(wordFreq map[string]int) map[string]int {
 	allWords := GetKeys(wordFreq)
-	// check whether the word without es or s exists in the map
-	// if true, remove this plural from the map
 	for w := range wordFreq {
+		// check whether the word without es or s exists in the map
+		// if true, remove this plural from the map
 		if strings.HasSuffix(w, "s") {
 			sChar := regexp.MustCompile(`s$`)
 			esChar := regexp.MustCompile(`es$`)
@@ -28,11 +28,22 @@ func HandlePlural(wordFreq map[string]int) map[string]int {
 				wordFreq[w] = 0
 			}
 		}
+
+		// check whether the word without ly exists in the map
+		// if true, remove this adverb from the map
+		if strings.HasSuffix(w, "ly") {
+			lyChar := regexp.MustCompile(`ly$`)
+			removeLy := lyChar.ReplaceAllString(w, "")
+			if StringInList(removeLy, allWords) {
+				wordFreq[removeLy] += wordFreq[w]
+				wordFreq[w] = 0
+			}
+		}
 	}
 
 	// remove words with 0 frequency by first creating a new map
 	// and add word with fequency that is not 0
-	newMap := make(map[string]int, 0)
+	newMap := make(map[string]int)
 	for k, v := range wordFreq {
 		if v != 0 {
 			newMap[k] = v
