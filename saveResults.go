a44764c28a524593a925f3b8d1f9d014ae195681
Wangxin555 <wangx555@mail2.sysu.edu.cn>
2021-11-24 19:09:07 -0500

    Add SaveWords function to save words in a txt file

--- saveResults.go
+++ saveResults.go
@@ -13,11 +13,26 @@ func SaveSearchResult(filename string, papers []Paper) {
 	}
 	defer outFile.Close()
 
+	// set header
 	fmt.Fprintln(outFile, "PMID"+"\t"+"Title"+"\t"+"Abstract")
 
 	for _, data := range papers {
 		fmt.Fprintln(outFile, data.PMID+"\t"+data.title+"\t",
 			data.abstract)
 	}
-	fmt.Println("Successfully write to txt file!")
+	fmt.Println("Successfully write search results to a txt file!")
+}
+
+// SaveWords save words in a txt file
+func SaveWords(words []string, filename string) {
+	outFile, err := os.Create(filename)
+	if err != nil {
+		panic("Sorry: cannot create the file!")
+	}
+	defer outFile.Close()
+
+	for _, w := range words {
+		fmt.Fprintln(outFile, w)
+	}
+	fmt.Println("Successfully write words to a txt file!")
 }
